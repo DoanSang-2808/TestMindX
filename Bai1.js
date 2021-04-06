5078acf84ccfa11f02c4442b9fafb04d03a1641a
Sang <doanthisang2808@gmail.com>
2021-04-06 23:38:35 +0700

    hi

--- Bai1.js
+++ Bai1.js
@@ -1,9 +1,9 @@
 function validTime(time){
     let h_m = time.split(":")
-    if(h_m[0] > 12 && h_m[0] < 25 ){
+    if(h_m[0] > 12 && h_m[0] <= 24 ){
         return true;
     } 
     return false;
     //console.log(h_m)
 }
-console.log(validTime("02:00"));
+console.log(validTime("13:09"));
